eca2c0be56ddb48f8d07cc9995da392fc382daea
Raymond <raymond@Derps.local>
2016-09-06 00:02:44 -0400

    add sample salt state, add extra flask server

---
+++ README.md
@@ -0,0 +1,6 @@
+Next steps:
+1) Add event handler for serf to launch salt --> add new members to haproxy if webserver
+2) Add templates/static files for flask
+3) Add more routes for flask
+4) Add backend logic / redis usage
+5) Add salt play to handle re-deployment
--- flask/code/app.py
+++ flask/code/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from redis import Redis
+import os
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
@@ -11,7 +12,7 @@ def count():
 
 @app.route('/')
 def hello():
-    return "Hey welcome howdy"
+    return "Hello! You're being served by: "+os.uname()[1]
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
